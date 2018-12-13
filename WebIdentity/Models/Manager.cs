d81c24f65ce74378a5008d8e2d84e62ad51fa820
Кожевников Валерий Викторович <Kozhevnikov.VVik@gazprom-neft.local>
2018-12-13 12:21:26 +0300

    Add models

---
+++ WebIdentity/Models/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebIdentity.Models
+{
+    public class Manager
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Position { get; set; }
+        public virtual List<Problem> Problems { get; set; }
+
+    }
+}
---
+++ WebIdentity/Models/Problem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebIdentity.Models
+{
+    public class Problem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateBegin { get; set; }
+        public DateTime DateEnd { get; set; }
+        public string Status { get; set; }
+        public virtual Manager Manager { get; set; }
+    }
+}
